de4197c93743daeccc5346bf05a67192f8c25f50
Theo Harris <theosaurus13@gmail.com>
2023-11-11 10:00:22 +1100

    Set up event handling for adding a new todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+
+import { Todo } from "./model";
 import InputField from "./components/InputField";
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+    }
+    setTodo("");
+  };
+
+  console.log(todo);
   return (
     <div className="App">
       <h1 className="heading">Taskly</h1>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input">
-      <input type="text" placeholder="Enter a task" className="input__box" />
-      <button className="input__submit" type="submit">
-        Go
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Enter a Task"
+        value={todo}
+        ref={inputRef}
+        onChange={(e) => setTodo(e.target.value)}
+        className="input__box"
+      />
+      <button type="submit" className="input__submit">
+        GO
       </button>
     </form>
   );
